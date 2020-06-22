a0adbb8dd49bac90cac294f5eae3501f0fe0ef0d
YuERA1992 <yuzaburo.era@jointcrew.co.jp>
2020-06-22 09:29:34 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// ここから追加
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faUser,
+  faSignOutAlt,
+  faEllipsisV
+} from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+  faUser,
+  faSignOutAlt,
+  faEllipsisV
+)
+
+Vue.component('fa', FontAwesomeIcon)
+// ここまで追加
+
 Vue.config.productionTip = false
 
 new Vue({
