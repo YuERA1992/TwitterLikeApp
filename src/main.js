2234e61a3004770e2e080cbe075280ba52670d0e
YuERA1992 <yuzaburo.era@jointcrew.co.jp>
2020-06-23 07:57:06 +0900

    Firebaseと連携、Home画面で投稿を表示、投稿を日付順に並び替える

--- src/main.js
+++ src/main.js
@@ -21,6 +21,28 @@ library.add(
 Vue.component('fa', FontAwesomeIcon)
 // ここまで追加
 
+// ここから追加
+import { firestorePlugin } from 'vuefire'          
+import firebase from 'firebase'     
+import 'firebase/firestore'
+
+Vue.use(firestorePlugin)
+
+firebase.initializeApp({                
+  apiKey: "AIzaSyBIk1PCEYse9mjd3HFb-9rkAsI_niEUuO4",
+  authDomain: "twitterlikeapp-66f83.firebaseapp.com",
+  databaseURL: "https://twitterlikeapp-66f83.firebaseio.com",
+  projectId: "twitterlikeapp-66f83",
+  storageBucket: "twitterlikeapp-66f83.appspot.com",
+  messagingSenderId: "1011578434602",
+  appId: "1:1011578434602:web:4e94f7aa55b6307a637f53",
+  measurementId: "G-0CG9MJMTS6"
+})
+
+export const db = firebase.firestore()
+export const auth = firebase.auth()
+// ここまで追加
+
 Vue.config.productionTip = false
 
 new Vue({
